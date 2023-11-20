3c4a606e03960df44485f211123f778f9b50906f
Cod3rMax <walid@MacBook-Pro-de-walid.local>
2023-11-20 01:49:24 +0100

    Starting fixing the css of the project

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+let CANVAS_WIDTH = (canvas.width =
+     document.getElementById('container').getBoundingClientRect().width / 2);
+let CANVAS_HEIGHT = (canvas.height =
+     document.getElementById('container').getBoundingClientRect().height / 2);
